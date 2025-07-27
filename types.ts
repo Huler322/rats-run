febf81dbb2c2c8480a4cd21da46ea04f64281f81
Anatoliy Tarnovskiy <tarnovskidj@gmail.com>
2025-07-27 16:02:36 +0200

    add user statuses

--- types.ts
+++ types.ts
@@ -7,3 +7,10 @@ export enum TypeNavigation {
 
   NOT_FOUND = '+not-found',
 }
+
+export enum UserStatus {
+  proccessing = 'processing',
+  created = 'created',
+  lost = 'lost',
+  win = 'win',
+}

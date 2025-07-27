7e39ff5d27cffc3834c99e32c7fc80d8c4243007
OdrinskayaMargarita <m.odrinskaya@gmail.com>
2025-07-27 23:09:32 +0200

    start components for rich circle

---
+++ components/rich-circle/rich-business-list.component.tsx
@@ -0,0 +1,10 @@
+import { View } from 'react-native';
+
+const richBusinessList = [
+  { id: 1, name: 'Hugo Boss', price: 275000, income: 12000 },
+  { id: 2, name: 'Zavod strausov', price: 333000, income: 33000 },
+];
+
+export const RichBusinessListComponent = () => {
+  return <View></View>;
+};

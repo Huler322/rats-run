356b44b2b185c5b42cd262f78e9fc9df6c4caf59
OdrinskayaMargarita <m.odrinskaya@gmail.com>
2025-07-31 20:22:55 +0200

    add form for add business on rich circle

--- app/(tabs)/richCircle.tsx
+++ app/(tabs)/richCircle.tsx
@@ -2,11 +2,16 @@ import { HeaderComponent } from '@/components/header/header.component';
 import { ContainerScrollComponent } from '@/components/templates/container-scroll.component';
 import { View } from 'react-native';
 import { RichBusinessListComponent } from '@/components/rich-circle/rich-business-list.component';
+import tw from '@/lib/tailwind';
+import { ButtonComponent } from '@/components/buttons/button.component';
+import { RichActionButtons } from '@/components/rich-circle/rich-action-buttons';
+import { RichAddBussinessForm } from '@/components/rich-circle/rich-add-bussiness-form';
 
 export default function RichCircleScreen() {
   return (
     <ContainerScrollComponent styles={'bg-white'} header={<HeaderComponent />}>
-      <View></View>
+      <RichActionButtons />
+      <RichAddBussinessForm />
       <RichBusinessListComponent />
     </ContainerScrollComponent>
   );
---
+++ components/rich-circle/rich-add-bussiness-form.tsx
@@ -0,0 +1,73 @@
+import { View } from 'react-native';
+import tw from '@/lib/tailwind';
+import { Controller } from 'react-hook-form';
+import { InputComponent } from '@/components/inputs/input.component';
+import { ButtonComponent } from '@/components/buttons/button.component';
+import { RowComponent } from '@/components/UI/row.component';
+import { useBuyStocks } from '@/hooks/useBuyStocks';
+
+export const RichAddBussinessForm = () => {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+    getValues,
+    // reset,
+  } = useBuyStocks();
+
+  const onBuyBusiness = () => {};
+
+  return (
+    <View style={tw`mb-2 mt-2`}>
+      <Controller
+        control={control}
+        render={({ field: { onChange, value } }) => (
+          <InputComponent
+            value={value}
+            onChange={onChange}
+            placeholder={'Hugo Boss'}
+            // error={errors.name}
+            label={'Business name'}
+          />
+        )}
+        name="name"
+        defaultValue=""
+      />
+      <RowComponent>
+        <View style={tw`w-[35%]`}>
+          <Controller
+            control={control}
+            render={({ field: { onChange, value } }) => (
+              <InputComponent
+                value={value}
+                onChange={onChange}
+                placeholder={'430900'}
+                // error={errors.name}
+                label={'Business price'}
+              />
+            )}
+            name="name"
+            defaultValue=""
+          />
+        </View>
+        <View style={tw`w-[35%]`}>
+          <Controller
+            control={control}
+            render={({ field: { onChange, value } }) => (
+              <InputComponent
+                value={value}
+                onChange={onChange}
+                placeholder={'125'}
+                // error={errors.name}
+                label={'Income'}
+              />
+            )}
+            name="name"
+            defaultValue=""
+          />
+        </View>
+        <ButtonComponent title="Buy" styles="w-[28%] px-2" onPress={handleSubmit(onBuyBusiness)} />
+      </RowComponent>
+    </View>
+  );
+};

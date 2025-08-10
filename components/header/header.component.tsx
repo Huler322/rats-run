c93759ce8a6fdd493e090dbff086c885a9d20bfa
OdrinskayaMargarita <m.odrinskaya@gmail.com>
2025-08-10 17:08:20 +0200

    formatted total alance

--- components/header/header.component.tsx
+++ components/header/header.component.tsx
@@ -11,11 +11,15 @@ import Decimal from 'decimal.js';
 export const HeaderComponent = () => {
   const { currentUser } = useAppSelector(({ game }) => game);
 
+  const formatCapitalWithSpaces = (num) => {
+    return currentUser.currentCapital.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
+  };
+
   return (
     <>
       <RowComponent styles="mb-1 p-2 rounded-md bg-gray-200">
         <Text style={tw`text-lg font-medium`}>Total Balance:</Text>
-        <Text style={tw`text-lg text-orange-500 font-bold`}>$ {currentUser.currentCapital}</Text>
+        <Text style={tw`text-lg text-orange-500 font-bold`}>$ {formatCapitalWithSpaces()}</Text>
       </RowComponent>
     </>
   );

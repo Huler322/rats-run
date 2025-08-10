747999bfc0f4e0f4e6468ebc86bbca1163a717e5
OdrinskayaMargarita <m.odrinskaya@gmail.com>
2025-08-10 13:47:42 +0200

    add alert on divorce and tax on rich circle

--- components/rich-circle/rich-action-buttons.tsx
+++ components/rich-circle/rich-action-buttons.tsx
@@ -1,10 +1,10 @@
-import { View } from 'react-native';
+import { Alert, View } from 'react-native';
 import tw from '@/lib/tailwind';
 import { ButtonComponent } from '@/components/buttons/button.component';
 import { FC } from 'react';
 import { IUser } from '@/store/types';
 import { useAppDispatch } from '@/store';
-import { setUserDivorced, minusTax } from '@/slices/game.slice';
+import { minusTax } from '@/slices/game.slice';
 
 export const RichActionButtons: FC<IProps> = ({ currentUser }) => {
   const dispatch = useAppDispatch();
@@ -12,10 +12,27 @@ export const RichActionButtons: FC<IProps> = ({ currentUser }) => {
   if (!currentUser) return <></>;
 
   const handleDivorce = () => {
-    dispatch(setUserDivorced(currentUser));
+    Alert.alert('Are you sure want to divorce?', 'After divorce you lost half from capital', [
+      {
+        style: 'Cancel',
+        text: 'Cancel',
+      },
+      { onPress: () => dispatch(minusTax(currentUser)), text: 'Divorce' },
+    ]);
   };
+
   const handleTax = () => {
-    dispatch(minusTax(currentUser));
+    Alert.alert(
+      'Are you sure want to pay tax?',
+      'You lose 10% of your capital when you pay taxes.',
+      [
+        {
+          style: 'Cancel',
+          text: 'Cancel',
+        },
+        { onPress: () => dispatch(minusTax(currentUser)), text: 'Pay Tax' },
+      ],
+    );
   };
 
   return (
